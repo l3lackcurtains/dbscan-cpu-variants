267282c71502e9ac7688cb5dca2aac5d87f416c4
Madhav Poudel <king.ov.universe@gmail.com>
2020-02-17 20:17:15 -0700

    DBSCAN implementation in C++

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include <vector>
 #include <algorithm>
 
-#define DATASET_SIZE 100
-#define ELIPSON 10
-#define MIN_POINTS 5
+#define DATASET_SIZE 500
+#define ELIPSON 20
+#define MIN_POINTS 10
 
 using namespace std;
 
 struct Point { 
-    int id, x, y;
+    int x, y;
 };
 
 struct Cluster {
@@ -19,6 +19,7 @@ struct Cluster {
 };
 
 class DBSCAN {
+
     private:
         Point dataset[DATASET_SIZE];
         int elipson;
@@ -28,13 +29,14 @@ class DBSCAN {
         int visited[DATASET_SIZE];
         vector<int> noises;
         int getDistance(int center, int neighbor);
+        vector<int> findNeighbors(int pos);
+        void expandCluster(int pointId, vector<int> &neighbors);
 
     public:
         DBSCAN(Point dataset[DATASET_SIZE]);
         void run();
-        vector<int> findNeighbors(int pos);
-        void expandCluster(int pointId, vector<int> &neighbors);
         void results();
+
 };
 
 int main(int, char **) {
@@ -45,147 +47,178 @@ int main(int, char **) {
     for(int i = 0; i < DATASET_SIZE; i++) {
         int x = rand() % 50;
         int y = rand() % 50;
-        dataset[i].id = i;
         dataset[i].x = x;
         dataset[i].y = y;
     }
     
-    printf("Random Dataset created \n###############################\n");
+    printf("Random Dataset created \n ############################### \n");
+
+    // Print dataset in an array structure
+    printf("[");
     for(int i = 0; i < DATASET_SIZE; i++) {
-        printf("\n%d: [%d, %d] \n", i, dataset[i].x, dataset[i].y);
+        printf("[%d, %d], ", dataset[i].x, dataset[i].y);
     }
+    printf("]\n");
+
     printf("############################### \n");
 
+    // Initialize DBSCAN with dataset
     DBSCAN dbscan(dataset);
 
+    // Run the DBSCAN algorithm
     dbscan.run();
 
+    // Print the cluster results of DBSCAN
     dbscan.results();
 
     return 0;
+
 }
 
 DBSCAN::DBSCAN(Point loadData[DATASET_SIZE]) {
+
     elipson = ELIPSON;
     minPoints = MIN_POINTS;
 
     for(int i = 0; i < DATASET_SIZE; i++) {
         dataset[i].x = loadData[i].x;
         dataset[i].y = loadData[i].y;
-        dataset[i].id = loadData[i].id;
         visited[i] = 0;
     }
+
 }
 
 int DBSCAN::getDistance(int center, int neighbor) {
+
     int dist = pow(dataset[center].x - dataset[neighbor].x, 2) + pow(dataset[center].y - dataset[neighbor].y, 2);
+
     return sqrt(dist);
+
 }
 
 void DBSCAN::run() {
+
+    // Neighbors of the point
     vector<int> neighbors;
+
+    // Cluster count
     clusterCount = 0;
 
     for (int i = 0; i < DATASET_SIZE; i++) {
-        if(visited[i] == 0) {
-            visited[i] = 1;
 
+        // Continue when P is not visited
+        if(visited[i] == 0) {
+
+            // Mark P as visited
+            visited[i] = 1;
+            
+            // Find neighbors of point P
             neighbors = findNeighbors(i);
             
+            // Mark noise points
             if (neighbors.size() < minPoints) {
-                
-                clusters[clusterCount].id = clusterCount;
-                clusters[clusterCount].data.push_back(i);
-
-            } else {
-
-                clusterCount++;
-
-                Cluster cluster;
-                cluster.id = clusterCount;
-                cluster.data = {};
-
-                clusters.push_back(cluster);
-                
-                expandCluster(i, neighbors);
-                
+                noises.push_back(i);
             }
+            
+            // Increment cluster and initialize it will the current point
+            clusterCount++;
+
+            Cluster cluster;
+            cluster.id = clusterCount;
+            cluster.data.push_back(i);
+
+            // Expand the neighbors of point P
+            for(int j = 0; j < neighbors.size(); j++) {
+
+                // Mark neighbour as point Q
+                int dataIndex = neighbors[j];
+
+                // If point is a noise push to the cluster
+                for(int k = 0; k < noises.size(); k++) {
+                    if(noises[k] == dataIndex)
+                        cluster.data.push_back(dataIndex);
+                }
+
+                // Continue when Q is not visited
+                if(visited[dataIndex] == 0) {
+
+                    // Mark Q as visited
+                    visited[dataIndex] = 1;
+
+                    // Check if point Q already exists in cluster
+                    bool notFound = false;
+                    for(int l = 0; l < cluster.data.size(); l++) {
+                        if(cluster.data[l] != dataIndex)
+                            notFound = true;
+                    }
+
+                    // If point Q doesn't exist in any cluster, add to current cluster
+                    if(notFound) {
+                        cluster.data.push_back(dataIndex);
+                    }
+                    
+                    // Expand more neighbors of point Q
+                    vector<int> moreNeighbors;
+                    moreNeighbors = findNeighbors(dataIndex);
+
+                    // Continue when neighbors point is higher than minPoint threshold
+
+                    if (moreNeighbors.size() >= minPoints) {
+
+                        // Check if neighbour of Q already exists in neighbour of P
+                        bool doesntExist = true;
+                        for(int x = 0; x < moreNeighbors.size(); x++) { 
+                            for(int y = 0; y < neighbors.size(); y++) {
+                                if(moreNeighbors[x] == neighbors[y]) {
+                                    doesntExist = false;
+                                }
+                            }
+
+                            // If neighbour doesn't exist, add to neighbor list
+                            if(doesntExist) {
+                                neighbors.push_back(moreNeighbors[x]);
+                            }
+
+                        }  
+                    }
+                }
+            }
+
+            // Push cluster into clusters vector
+            clusters.push_back(cluster);
+
         }
     }
-
     
 }
 
 void DBSCAN::results() {
+
     for(int j = 0; j < clusters.size(); j++) {
         printf("Data for cluster %d \n", j);
+        printf("[\n");
         for(int k = 0; k < clusters[j].data.size(); k++) {
-            printf("Point [%d, %d] \n", dataset[clusters[j].data[k]].x, dataset[clusters[j].data[k]].y);
+            printf("  [%d, %d]\n", dataset[clusters[j].data[k]].x, dataset[clusters[j].data[k]].y);
         }
+        printf("]\n");
     }
+
 }
 
 vector<int> DBSCAN::findNeighbors(int pos) {
 
     vector<int> neighbors;
-
-    int neighborCount = 0;
     
     for (int x = 0; x < DATASET_SIZE; x++) {
-        int distance = getDistance(pos, x);
 
-        if (distance < elipson) {
+        // Compute neighbor points of a point at position "pos"
+        int distance = getDistance(pos, x);
+        if (distance <= elipson && pos != x) {
             neighbors.push_back(x);
         }
-    }
 
-    
-    printf("Neighbor of dataset [%d, %d]", dataset[pos].x, dataset[pos].y);
-
-    for(int i = 0; i < neighbors.size(); i++) {
-        printf("\n%d: [%d, %d] \n", neighbors[i], dataset[neighbors[i]].x, dataset[neighbors[i]].y ); 
     }
-    
 
     return neighbors;
-}
 
-void DBSCAN::expandCluster(int pointId, vector<int> &neighbors) {
-
-    
-
-    clusters[clusterCount].data.push_back(pointId);
-
-    for(int b = 0; b < clusters.size(); b++) {
-        cout<<clusters[b].data.size()<<endl;
-    }
-
-    cout<<"here!"<<endl;
-
-    vector<int> moreNeighbors;
-    
-    for(int i = 0; i < neighbors.size(); i++) {
-        if (visited[i] == 0) {
-            visited[i] = 1;
-
-            moreNeighbors = findNeighbors(i);
-            int moreNeighborSize = moreNeighbors.size();
-
-            if(moreNeighborSize >= minPoints) {
-
-                for(int x = 0; x < moreNeighborSize; x++) {                    
-                    neighbors.push_back(moreNeighbors[x]);
-                }
-            }
-
-            for(int x = 0; x < clusterCount; x++) {
-                for(int y = 0; y < clusters[x].data.size(); y++) {
-                    
-                    if(clusters[x].data[y] != i) { 
-                        clusters[clusterCount].data.push_back(i);
-                    }
-                } 
-            }
-        }
-    }
 }
